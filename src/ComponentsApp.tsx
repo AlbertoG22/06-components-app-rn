3beb83000a5d5b536f1eda7aba5ab81cfd024a07
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-10-28 11:11:32 -0600

    instalar iconos

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { HomeScreen } from './presentation/screens/home/HomeScreen';
 
 export const ComponentsApp = () => {
   return (
     <View>
-      <Text>Components App</Text>      
+      <Text>Components App</Text>
+      <HomeScreen />
     </View>
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { AirplaneIcon } from '../../icons/Icons';
 
 export const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text> 
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
+      <AirplaneIcon />
     </View>
   );
 };
