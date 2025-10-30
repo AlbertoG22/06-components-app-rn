88413090dd2c88099b2e84eb0bae6bacb6c3aad1
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-10-30 12:50:11 -0600

    componente - switch

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Switch, Text, View } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Card } from '../../components/ui/Card';
 import { Button } from '../../components/ui/Button';
+import { CustomSwitch } from '../../components/ui/CustomSwitch';
+import { isEnabled } from 'react-native/Libraries/Performance/Systrace';
 
 export const SwitchScreen = () => {
+
+  // const [isEnabled, setIsEnabled] = useState(false);
+  // const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+  const [ state, setState ] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
   return (
     <CustomView style={{ marginTop: 100, paddingHorizontal: 10 }}>
       <Card>
-        {/* <Text>SwitchScreen</Text> */}
-        <Button text='Click me' onPress={ () => {} } />
+        <CustomSwitch 
+          isOn={ state.isActive }
+          onChange={ (value) => setState({ ...state, isActive: value }) }
+          text='Está activo?'
+        />
+
+        <CustomSwitch 
+          isOn={ state.isHungry }
+          onChange={ (value) => setState({ ...state, isHungry: value }) }
+          text='Tiene hambre?'
+        />
+
+        <CustomSwitch 
+          isOn={ state.isHappy }
+          onChange={ (value) => setState({ ...state, isHappy: value }) }
+          text='Es feliz?'
+        />
       </Card>
     </CustomView>
+    
   );
 };
