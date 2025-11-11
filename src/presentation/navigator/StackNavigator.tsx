4c05d3da91d04442a1247136aa4ca867c192c513
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-11-10 20:16:06 -0600

    carousel - Slideshow pt.1

--- src/presentation/navigator/StackNavigator.tsx
+++ src/presentation/navigator/StackNavigator.tsx
@@ -9,6 +9,7 @@ import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
 import { CustomSectionListScreen } from '../screens/ui/CustomSectionListScreen';
 import { ModalScreen } from '../screens/ui/ModalScreen';
 import { InfiniteScrollScreen } from '../screens/ui/InfiniteScrollScreen';
+import { SlidesScreen } from '../screens/ui/SlidesScreen';
 
 const Stack = createStackNavigator();
 
@@ -29,6 +30,7 @@ export const StackNavigator = () => {
             <Stack.Screen name='SwitchScreen' component={SwitchScreen} />
             <Stack.Screen name='AlertScreen' component={AlertScreen} />
             <Stack.Screen name='TextInputScreen' component={TextInputScreen} />
+            <Stack.Screen name='SlidesScreen' component={SlidesScreen} />
         </Stack.Navigator>
     );
 };
