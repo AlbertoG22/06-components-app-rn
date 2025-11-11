b7a16e89eeaa0ffd24ffb3ddf1c1e5f3c2063308
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-11-11 11:31:02 -0600

    theme light & dark pt.2 (ThemeContext)

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -3,11 +3,21 @@ import { Text, View } from 'react-native';
 import { HomeScreen } from './presentation/screens/home/HomeScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
+import { PropsWithChildren } from 'react';
+import { ThemeProvider } from './presentation/context/ThemeContext';
+
+const AppState = ({ children }: PropsWithChildren) => {
+  return(
+    <NavigationContainer>
+      <ThemeProvider>{ children }</ThemeProvider>
+    </NavigationContainer>
+  );
+};
 
 export const ComponentsApp = () => {
   return (
-    <NavigationContainer>
+    <AppState>
       <StackNavigator />
-    </NavigationContainer>
+    </AppState>
   );
 };
--- src/presentation/screens/theme/ChangeThemeScreen.tsx
+++ src/presentation/screens/theme/ChangeThemeScreen.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { CustomView } from '../../components/ui/CustomView';
 import { Title } from '../../components/ui/Title';
 import { Button } from '../../components/ui/Button';
 import { View } from 'react-native';
+import { ThemeContext } from '../../context/ThemeContext';
 
 export const ChangeThemeScreen = () => {
+
+  const { setTheme } = useContext( ThemeContext );
+
   return (
     <CustomView margin>
       <Title text='Cambiar tema' safe />
 
       <Button
         text='Light'
-        onPress={ () => {} }
+        onPress={ () => setTheme('light') }
       />
 
       <View style={{ height: 10 }} />
 
       <Button
         text='Dark'
-        onPress={ () => {} }
+        onPress={ () => setTheme('dark') }
       />
 
     </CustomView>
