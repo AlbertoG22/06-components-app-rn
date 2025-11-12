bbdd5586540029f1c90080916cc0450653e2ab67
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-11-12 13:32:02 -0600

    react navigation theme

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
 import { HomeScreen } from './presentation/screens/home/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
 import { PropsWithChildren } from 'react';
-import { ThemeProvider } from './presentation/context/ThemeContext';
+import { ThemeContext, ThemeProvider } from './presentation/context/ThemeContext';
 
-const AppState = ({ children }: PropsWithChildren) => {
+const AppNavigation = ({ children }: PropsWithChildren) => {
+  const { isDark } = useContext(ThemeContext);
+
   return(
-    <NavigationContainer>
-      <ThemeProvider>{ children }</ThemeProvider>
+    <NavigationContainer theme={ isDark ? DarkTheme : DefaultTheme }>
+      { children }
     </NavigationContainer>
+  );
+};
+
+const AppTheme = ({ children }: PropsWithChildren) => {
+  return(
+    <ThemeProvider>
+      <AppNavigation>{children}</AppNavigation>
+    </ThemeProvider>
   );
 };
 
 export const ComponentsApp = () => {
   return (
-    <AppState>
+    <AppTheme>
       <StackNavigator />
-    </AppState>
+    </AppTheme>
   );
 };
