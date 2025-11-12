2f9e42da2cd261afe6493a6d1ab1d57d55907bad
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-11-12 16:32:10 -0600

    simplificación los componentes

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,33 +1,10 @@
-import React, { useContext } from 'react';
-import { Text, View } from 'react-native';
-import { HomeScreen } from './presentation/screens/home/HomeScreen';
-import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
-import { PropsWithChildren } from 'react';
-import { ThemeContext, ThemeProvider } from './presentation/context/ThemeContext';
-
-const AppNavigation = ({ children }: PropsWithChildren) => {
-  const { isDark } = useContext(ThemeContext);
-
-  return(
-    <NavigationContainer theme={ isDark ? DarkTheme : DefaultTheme }>
-      { children }
-    </NavigationContainer>
-  );
-};
-
-const AppTheme = ({ children }: PropsWithChildren) => {
-  return(
-    <ThemeProvider>
-      <AppNavigation>{children}</AppNavigation>
-    </ThemeProvider>
-  );
-};
+import { ThemeProvider } from './presentation/context/ThemeContext';
 
 export const ComponentsApp = () => {
   return (
-    <AppTheme>
+    <ThemeProvider>
       <StackNavigator />
-    </AppTheme>
+    </ThemeProvider>
   );
 };
--- src/presentation/context/ThemeContext.tsx
+++ src/presentation/context/ThemeContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, PropsWithChildren, useEffect, useState } from 'react';
 import { darkColors, lightColors, ThemeColors } from '../../config/theme/theme';
 import { Appearance, AppState, useColorScheme } from 'react-native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 type ThemeColor = 'light' | 'dark';
 
@@ -18,6 +19,9 @@ export const ThemeContext = createContext({} as ThemeContextProps);
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
   const colorScheme = useColorScheme();
   const [currentTheme, setCurrentTheme] = useState<ThemeColor>('light');
+
+  const isDark = currentTheme === 'dark';
+  const colors = isDark ? darkColors : lightColors
 
   // OPCIÓN 1 => (useColorScheme)
   useEffect( () => {
@@ -45,15 +49,17 @@ export const ThemeProvider = ({ children }: PropsWithChildren) => {
   };
 
   return(
-    <ThemeContext.Provider
-      value={{
-        currentTheme: currentTheme,
-        isDark: (currentTheme !== 'light'),
-        colors: (currentTheme === 'light' ? lightColors : darkColors),
-        setTheme: setTheme,
-      }}
-    >
-      { children }
-    </ThemeContext.Provider>
+    <NavigationContainer theme={ isDark ? DarkTheme : DefaultTheme }>
+      <ThemeContext.Provider
+        value={{
+          currentTheme: currentTheme,
+          isDark: isDark,
+          colors: colors,
+          setTheme: setTheme,
+        }}
+      >
+        { children }
+      </ThemeContext.Provider>
+    </NavigationContainer>
   );
 };
